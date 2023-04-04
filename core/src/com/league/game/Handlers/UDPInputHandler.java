bb478b83eda10ebb0b139372dd161a6c2feaafc4
hmongkey12 <khammadaolee@gmail.com>
2023-04-04 10:43:47 -0700

    [documentation] added documentation to UDPInputHandler

--- core/src/com/league/game/Handlers/UDPInputHandler.java
+++ core/src/com/league/game/Handlers/UDPInputHandler.java
@@ -9,6 +9,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UDPInputHandler {
+
+    /**
+     * Handles input received from the user's keyboard and sends commands to the game server based on the keyboard keys pressed.
+     * @param gameManager the main game manager that contains the reference to the UDP network handler. NetworkHandler will be the connection between client and server.
+     * The method sends commands to move the player character left, right, up, down, and to use the first skill, as specified by the keyboard keys A, D, W, S, and J respectively.</p>
+     * If the UDP network handler is null, no command is sent.</p>
+     */
     public static void handleInput(LeagueOfHorrors gameManager) {
         if (gameManager.udpNetworkHandler == null) {
             return;
@@ -30,6 +37,12 @@ public class UDPInputHandler {
         }
     }
 
+    /**
+     * Sends a command to the game server via UDP, which is the user's input from the keyboard.
+     * @param gameManager the main game manager that contains the reference to the UDP network handler. NetworkHandler will be the connection between client and server.
+     * @param commandType the type of the command to be sent to the game server. It can be one of the following values: "left", "right", "up", "down", or "skill1".
+     * If the UDP network handler is null, the method returns without sending any command.</p>
+     */
     private static void sendCommand(LeagueOfHorrors gameManager, String commandType) {
         Map<String, String> command = new HashMap<>();
         command.put("command", commandType + "_" + gameManager.getPlayerId());
