a307e1aca05feefbba35b936feaeb1a06b16cb44
hmongkey12 <khammadaolee@gmail.com>
2023-04-04 10:27:00 -0700

    [refactor] refactored UDPInputHandler for maintanability

--- core/src/com/league/game/Handlers/UDPInputHandler.java
+++ core/src/com/league/game/Handlers/UDPInputHandler.java
@@ -9,39 +9,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UDPInputHandler {
-
     public static void handleInput(LeagueOfHorrors gameManager) {
-        Map<String, String> command = new HashMap<String, String>();
+        if (gameManager.udpNetworkHandler == null) {
+            return;
+        }
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            if (gameManager.getUdpNetworkHandler() != null) {
-                command.put("command", "left_" + gameManager.getPlayerId());
-                gameManager.udpNetworkHandler.sendData(JSONObject.toJSONString(command));
-            }
+            sendCommand(gameManager, "left");
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            if (gameManager.udpNetworkHandler != null) {
-                command.put("command", "right_" + gameManager.getPlayerId());
-                gameManager.udpNetworkHandler.sendData(JSONObject.toJSONString(command));
-            }
+            sendCommand(gameManager, "right");
         }
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            if (gameManager.udpNetworkHandler != null) {
-                command.put("command", "up_" + gameManager.getPlayerId());
-                gameManager.udpNetworkHandler.sendData(JSONObject.toJSONString(command));
-            }
+            sendCommand(gameManager, "up");
         }
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            if (gameManager.udpNetworkHandler != null) {
-                command.put("command", "down_" + gameManager.getPlayerId());
-                gameManager.udpNetworkHandler.sendData(JSONObject.toJSONString(command));
-            }
+            sendCommand(gameManager, "down");
         }
         if (Gdx.input.isKeyPressed(Input.Keys.J)) {
-            if (gameManager.udpNetworkHandler != null) {
-                command.put("command", "skill1_" + gameManager.getPlayerId());
-                gameManager.udpNetworkHandler.sendData(JSONObject.toJSONString(command));
-            }
+            sendCommand(gameManager, "skill1");
         }
     }
 
+    private static void sendCommand(LeagueOfHorrors gameManager, String commandType) {
+        Map<String, String> command = new HashMap<>();
+        command.put("command", commandType + "_" + gameManager.getPlayerId());
+        gameManager.udpNetworkHandler.sendData(JSONObject.toJSONString(command));
+    }
 }
